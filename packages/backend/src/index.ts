bceae82f6f620e75fc05cb7a13fd90f86db05e8e
Reza Karamad <r.karamad@gmail.com>
2026-07-17 13:05:36 +0200

    feat: add MSGraph instead of static user/groups

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -32,6 +32,7 @@ backend.add(import('@backstage/plugin-catalog-backend'));
 backend.add(
   import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
+backend.add(import('@backstage/plugin-catalog-backend-module-msgraph'));
 
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
